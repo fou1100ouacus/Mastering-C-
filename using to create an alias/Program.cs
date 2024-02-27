ee8da626f530817eeece40ff4ee23c5363a6597e
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-02-27 10:46:06 +0200

    Add files via upload

---
+++ using to create an alias/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+namespace using_to_create_an_alias
+{
+    // creating alias for System.Console
+    using Koko = System.Console;
+
+    namespace HelloWorld
+    {
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                double n = Sqrt(9);
+                Console.WriteLine("Square root of 9 is " + n);
+
+                // using Koko alias instead of System.Console
+                Koko.WriteLine("Hello World!");
+                Koko.ReadKey();
+            }
+        }
+    }
+
+}
