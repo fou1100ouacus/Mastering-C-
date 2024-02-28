7b22a3128230fdc3c85aba43975838e7393abced
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-02-28 11:45:44 +0200

    Add files via upload

---
+++ Lambda Expression/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression
+{
+    public class Program
+    {
+        // delegate int SquareDelegate(int x);
+                    
+        static Func<int, int> square = x => x * x;
+
+        static void Main(string[] args)
+        {
+            // SquareDelegate square = new SquareDelegate(SquareMethod);
+
+            int result = square(5);        
+                                                            
+            Console.WriteLine("The Square of 5 is: " + result);
+            Console.ReadLine();
+        } 
+    }
+
+}
