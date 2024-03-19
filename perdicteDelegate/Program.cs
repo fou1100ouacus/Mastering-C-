aaab95d5a1be400936c02cc8197e27f4df698fc2
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-03-19 15:23:07 +0200

    Add files via upload

---
+++ perdicteDelegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perdicteDelegate
+{
+    
+   
+        public class Program
+        { 
+             static Predicate<int> isEvenPredicate = isEven ;
+
+            static bool isEven(int x)
+            {
+                return x % 2 == 0;
+            }
+            static void Main(string[] args)
+            {
+                bool result = isEvenPredicate(5);
+
+                Console.WriteLine(result);
+
+                Console.ReadLine();
+            }
+
+
+        }
+    
+
+
+}
