24afa20fb84995cce29804f1ae0813f7f41a1b80
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-04-23 09:58:21 +0200

    Add files via upload

---
+++ Conditional Attribute Examples/Program.cs
@@ -0,0 +1,58 @@
+﻿//#define TRACE_ENABLED
+
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Conditional_Attribute_Examples
+{
+    internal class Program
+    {
+        public class MyClass
+        {
+            [Conditional("DEBUG")]
+            public void DebugMethod()
+            {
+                Console.WriteLine("Debug method executed.");
+            }
+
+
+            public void NormalMethod()
+            {
+                Console.WriteLine("Normal method executed.");
+            }
+        }
+        public class TraceExample
+        {
+          [Conditional("TRACE_ENABLED")]
+            public static void LogTrace(string message)
+            {
+                Console.WriteLine($"[TRACE] {message}");
+            }
+
+
+            static void Main(string[] args)
+            {
+                MyClass myClass = new MyClass();
+
+
+                // Call the methods
+                myClass.DebugMethod();  // This will only be executed in DEBUG builds
+                myClass.NormalMethod(); // This will always be executed
+
+                ////////////
+                ///
+                LogTrace("lllllllllabbbbbb");
+                
+                //     LogTrace("This trace message will only be included if TRACE_ENABLED is defined.");
+
+                //  example.lo
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Navigate String Library Using Reflection Example/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navigate_String_Library_Using_Reflection_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // Get the assembly containing the System.String type
+            Assembly mscorlib = typeof(string).Assembly;
+
+
+            // Get the System.String type
+            Type stringType = mscorlib.GetType("System.String");
+
+
+            if (stringType != null)
+            {
+                Console.WriteLine($"Methods of the System.String class:\n");
+
+
+                // Get all public methods of the System.String class
+                var stringMethods = stringType.GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                    .OrderBy(method => method.Name);
+
+
+                foreach (var method in stringMethods)
+                {
+                    Console.WriteLine($"\t{method.ReturnType} {method.Name}({GetParameterList(method.GetParameters())})");
+                }
+            }
+            else
+            {
+                Console.WriteLine("System.String type not found.");
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+
+        static string GetParameterList(ParameterInfo[] parameters)
+        {
+            return string.Join(", ", parameters.Select(parameter => $"{parameter.ParameterType} {parameter.Name}"));
+        }
+    }
+}
