8a5c024abe0f31e762a9407b6416daf2372ded29
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-05-10 15:58:56 +0300

    Add files via upload

---
+++ Custom Attribute/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Attribute
+{
+    internal class Program
+    {
+        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+        public class MyCustomAttribute : Attribute
+        {
+            public string Description { get; }
+
+
+            public MyCustomAttribute(string description)
+            {
+                Description = description;
+            }
+        }
+
+
+        [MyCustom("This is a class attribute")]
+        class MyClass
+        {
+            [MyCustom("This is a method attribute")]
+            public void MyMethod()
+            {
+                Console.WriteLine("hdfjkhkdjfh");
+                // Method implementation
+            }
+        }
+        static void Main(string[] args)
+        {
+            //You can then use reflection or other mechanisms to access and utilize the information
+            //    provided by these custom attributes at runtime.Custom attributes are often 
+            //    used for various purposes such as code generation, documentation, or influencing the behavior of frameworks and libraries.
+
+
+
+        }
+    }
+}
---
+++ Enumerators Iterators/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerators_Iterators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee { Id=1,Name="AYA", salary=937,Department="Engineer"};
+            Employee e2 = new Employee { Id = 1, Name = "AYA", salary = 937, Department = "Engineer" };
+            Employee e3 = e1; // e3 refer to reference of e1  then there has same reference
+            /////// --------------- before using Operator overloading -------------------------
+            Console.WriteLine(e1==e2);// false    compare refrence except string ==> compare content 
+            Console.WriteLine(e1==e3);// True
+            string s1 = "aya";
+            string s2 = "aya";
+            Console.WriteLine(s1==s2);// true 
+
+            Console.WriteLine(e1.Equals(e2));//before override Equals, get ==> false because as default compare references 
+            Console.WriteLine(e1.Equals(e2)); // true
+            /////// --------------- After using Operator overloading -------------------------
+            Console.WriteLine(e1==e2);// using  operator overloading 
+            Console.WriteLine(e1==e3);// using  operator overloading 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+
+
+
+        }
+       
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal salary { get; set; }
+            public string Department { get; set; }
+            public override bool Equals(object obj)
+            {
+                if (obj == null || !(obj is Employee))
+                {
+                    return false;
+                }
+                var emp = (Employee)obj;
+                return (
+                      this.Id == emp.Id &&
+                      this.Name == emp.Name &&
+                      this.Department == emp.Department &&
+                      this.salary == emp.salary
+                      ) ;
+           // return base.Equals(obj);
+        }
+        
+            public static bool operator ==(Employee lhs,Employee rhs  ) =>lhs.Equals(rhs);
+            public static bool operator !=(Employee lhs, Employee rhs) => !lhs.Equals(rhs);
+
+            public override int GetHashCode()
+            {
+
+
+                int hash = 13;// reduce the probability that 2 objects take the same content
+                hash= hash*7+Id.GetHashCode();
+                hash= hash*7+Name.GetHashCode();
+                hash=hash*7+Department.GetHashCode();
+                hash=hash*7+salary.GetHashCode();
+                return hash;
+            }
+
+        }
+    }
+}
