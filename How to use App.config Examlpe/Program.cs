1211f6ca4dff4924565939ac40f4df4be93fcbd2
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-05-12 00:05:23 +0300

    Add files via upload

---
+++ How to use App.config Examlpe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace How_to_use_App.config_Examlpe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+            string logLevel = ConfigurationManager.AppSettings["LogLevel"];
+            string koko = ConfigurationManager.AppSettings["koko"];
+
+            string MyDbConnection = ConfigurationManager.ConnectionStrings["MyDbConnection"].ConnectionString;
+
+            Console.WriteLine("\nConnectionString = " + connectionString);
+            Console.WriteLine("\nlogLevel = " + logLevel);
+            Console.WriteLine("\nkoko = " + koko);
+
+            Console.WriteLine("\nMyDbConnection = " + MyDbConnection);
+
+            Console.ReadKey();
+
+        }
+    }
+}
