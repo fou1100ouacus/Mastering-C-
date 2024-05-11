5feccb2c1e3fee21082db11e08b512530b79da83
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-05-11 23:46:36 +0300

    Add files via upload

---
+++ Properites/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properites
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class DOllar
+    {
+        private decimal _amount;
+        public decimal amount
+        {
+            get { return _amount; }
+            set
+            {
+                if (value < 0)
+
+
+                    _amount = 0;
+                else
+                    _amount = value;
+
+
+
+
+            }
+
+
+        }
+
+
+
+
+    }
+
+
+
+}
