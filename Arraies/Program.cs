9c5106d1567e0ac9a7083de12027e38732bc1763
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-07-05 17:37:21 +0300

    Add files via upload

---
+++ Arraies/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[3];
+            array[0] = "aya0";
+            array[1] = "aya1";
+            array[2] = "aya2";
+
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            //////
+            int[] array2 = new int[3] {1,2,3 };
+            foreach (var item in array2)
+            {
+                Console.WriteLine(item);
+            }
+            ///// the performace of new array slices2 is better than array [time life span]
+          //    var slices = 2..^2;
+          //  var slices2 = array[slices];
+
+            //var slices = array[1..2];starting from 1 take until 2 ==> 3
+
+
+
+        }
+    }
+}
---
+++ Implicit_ExplicitCasting/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit_ExplicitCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 34;
+            long num2 = num; // implicit
+            long nl = 1000;
+            if(nl <= Int32.MaxValue )
+            {
+                int n2 =(int)nl;
+
+
+            }
+            // converting form min to max is always a problem
+            int a = 3; //value type
+            object obj;// reference
+            obj =a ;// converting complicity from datatype value to reference
+            int b = (int)obj; // unboxing
+            // int to string
+            string str = "434343544069843";
+            if(int.TryParse(str,out int number))
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("invalid number ");
+            }
+        }
+    }
+}
